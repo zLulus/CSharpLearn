89d0f6d5da02690909f48064fd90459176ac0cd2
Lulus <865514198@qq.com>
2020-02-12 11:02:40 +0800

    Exhaustion

---
+++ CSharpGrammarLearnConsole/CSharpGrammarLearnConsole/Exhaustion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpGrammarLearnConsole
+{
+    internal static class Exhaustion
+    {
+        internal static void GetNumberOfBoysAndGirls1()
+        {
+            for(int boysNumber=0; boysNumber<=156; boysNumber++)
+            {
+                var girlsNumber = 156 - boysNumber;
+                //双等号表示“等于”
+                //10.0/ 11.0不能改成10/ 11，涉及精度问题
+                if ((10.0/ 11.0)  * boysNumber   == ((girlsNumber - 12) * 2))
+                {
+                    Console.WriteLine($"男生人数为{boysNumber}，女生人数为{girlsNumber}");
+                    break;
+                }
+            }
+            Console.WriteLine("运算完毕");
+        }
+
+        internal static void GetNumberOfBoysAndGirls2()
+        {
+            for (int boysNumber = 0; boysNumber <= 156; boysNumber++)
+            {
+                for(int girlsNumber=0; girlsNumber<=156; girlsNumber++)
+                {
+                    if ((girlsNumber + boysNumber == 156) &&
+                        (10.0 / 11.0) * boysNumber == ((girlsNumber - 12) * 2))
+                    {
+                        Console.WriteLine($"男生人数为{boysNumber}，女生人数为{girlsNumber}");
+                    }
+                }
+            }
+            Console.WriteLine("运算完毕");
+        }
+
+        internal static void GetArmstrongNumber(int startIndex,int endIndex)
+        {
+            if(startIndex> endIndex)
+            {
+                Console.WriteLine($"输入的数据错误，起始数据大于终止数据");
+                return;
+            }
+            Console.WriteLine($"获得{startIndex}-{endIndex}的水仙花数:");
+            for(var target= startIndex; target <= endIndex; target++)
+            {
+                //也可以通过取余的方式获得数字的每一位
+                var numberList = target.ToString().ToCharArray();
+                double sum = 0;
+                for (var index = 0; index < numberList.Length; index++)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(numberList[index]);
+                    string s = sb.ToString();
+                    var number = int.Parse(s);
+                    sum += Math.Pow(number, 3);
+                }
+                if(sum== target)
+                {
+                    Console.WriteLine($"水仙花数：{target}");
+                }
+            }
+            Console.WriteLine("运算完毕");
+        }
+        
+        internal static void Precision()
+        {
+            var result1 = 10 / 11;
+            Console.WriteLine($"10 / 11={result1}");
+            var result2 = 10.0 / 11.0;
+            Console.WriteLine($"10.0 / 11.0={result2}");
+        }
+    }
+}
--- CSharpGrammarLearnConsole/CSharpGrammarLearnConsole/Program.cs
+++ CSharpGrammarLearnConsole/CSharpGrammarLearnConsole/Program.cs
@@ -15,9 +15,16 @@ namespace CSharpGrammarLearnConsole
             //CumulativeSum.Method4();
             //CumulativeSum.Method5();
 
-            
+            //光明小学六年级选出的男生的1/11和12名女生参加数学竞赛，剩下的男生人数是剩下的女生人数的2倍.已知六年级共有156人，问男、女生各有多少人？
+            //Exhaustion.GetNumberOfBoysAndGirls1();
+            //Exhaustion.GetNumberOfBoysAndGirls2();
 
-            
+            //输出所有的“水仙花数”，所谓“水仙花数”是指一个三位数，其中各位数字立方和等于该数字本身。例如：153是一个“水仙花数”，因为153=1*1*1+5*5*5+3*3*3。
+            //Exhaustion.GetArmstrongNumber(100,999);
+
+            //精度
+            Exhaustion.Precision();
+
             Console.ReadLine();
         }
 
