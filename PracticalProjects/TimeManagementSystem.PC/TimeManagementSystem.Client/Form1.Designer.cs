a9f2f87018a4c40ec4d587cf6a7cbe18fbc6b00c
Lulu <865514198@qq.com>
2020-12-26 00:08:43 +0800

    列出了时间管理器界面，丰富了新增、删除键内容。

---
+++ PracticalProjects/TimeManagementSystem.PC/TimeManagementSystem.Client/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace TimeManagementSystem.PC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TheTaskList = new System.Windows.Forms.ListBox();
+            this.DetailEditAddButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.CompleteButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TheTaskList
+            // 
+            this.TheTaskList.FormattingEnabled = true;
+            this.TheTaskList.ItemHeight = 12;
+            this.TheTaskList.Location = new System.Drawing.Point(34, 47);
+            this.TheTaskList.Name = "TheTaskList";
+            this.TheTaskList.Size = new System.Drawing.Size(586, 352);
+            this.TheTaskList.TabIndex = 0;
+            this.TheTaskList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // DetailEditAddButton
+            // 
+            this.DetailEditAddButton.Location = new System.Drawing.Point(667, 47);
+            this.DetailEditAddButton.Name = "DetailEditAddButton";
+            this.DetailEditAddButton.Size = new System.Drawing.Size(112, 47);
+            this.DetailEditAddButton.TabIndex = 5;
+            this.DetailEditAddButton.Text = "详情/编辑/新增";
+            this.DetailEditAddButton.UseVisualStyleBackColor = true;
+            this.DetailEditAddButton.Click += new System.EventHandler(this.DetailEditAddButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(667, 150);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(112, 47);
+            this.DeleteButton.TabIndex = 6;
+            this.DeleteButton.Text = "删除";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // CompleteButton
+            // 
+            this.CompleteButton.Location = new System.Drawing.Point(667, 352);
+            this.CompleteButton.Name = "CompleteButton";
+            this.CompleteButton.Size = new System.Drawing.Size(112, 47);
+            this.CompleteButton.TabIndex = 8;
+            this.CompleteButton.Text = "完成";
+            this.CompleteButton.UseVisualStyleBackColor = true;
+            this.CompleteButton.Click += new System.EventHandler(this.Complete_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(667, 248);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(112, 47);
+            this.StopButton.TabIndex = 9;
+            this.StopButton.Text = "中止";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.CompleteButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.DetailEditAddButton);
+            this.Controls.Add(this.TheTaskList);
+            this.Name = "Form1";
+            this.Text = "时间管理";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox TheTaskList;
+        private System.Windows.Forms.Button DetailEditAddButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button CompleteButton;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ PracticalProjects/TimeManagementSystem.PC/TimeManagementSystem.Client/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeManagementSystem.PC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            AddData();
+        }
+
+        private void AddData()
+        {
+            //新增
+            for(int i = 0; i < 10; i++)
+            {
+                this.TheTaskList.Items.Add($"任务{i}");
+            }
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //todo 根据任务状态控制按钮的可用与不可用
+            if (this.TheTaskList.SelectedIndex == 0)
+            {
+                DeleteButton.Enabled = false;
+            }
+            else
+            {
+                DeleteButton.Enabled = true;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Complete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DetailEditAddButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            
+
+            if (this.TheTaskList.SelectedItem == null)
+            {
+                MessageBox.Show("未选择项目，无法删除！");
+                return;
+            }
+            var selectItem = this.TheTaskList.SelectedItem as string;
+            var r= MessageBox.Show($"是否确定删除{selectItem}？","1111",MessageBoxButtons.YesNo,MessageBoxIcon.Information);
+            if (r == DialogResult.Yes)
+            {
+                this.TheTaskList.Items.Remove(selectItem);
+            }
+        }
+    }
+}
